c0faa6d5d4dfb5d4c92fab23b306c4638755ee1d
Dimitar Kisov <mitko9737@gmail.com>
2025-11-11 14:24:05 +0200

    Added ValidationBehaviour and ServiceCollectionExtensions

---
+++ Body4uHUB.Content.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Body4uHUB.Content.Application.Extensions
+{
+    using Body4uHUB.Content.Application.Behaviours;
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Reflection;
+    using System.Text;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>))
+                .AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+                .AddJwtAuthentication(configuration);
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtIssuer = configuration["JwtSettings:Issuer"];
+            var jwtAudience = configuration["JwtSettings:Audience"];
+            var jwtSecret = configuration["JwtSettings:Secret"];
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+                };
+            });
+
+            return services;
+        }
+    }
+}
