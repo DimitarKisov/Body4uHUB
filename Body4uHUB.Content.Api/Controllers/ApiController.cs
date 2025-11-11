c9b993231a6277092077f32b7d9c943e36716e33
Dimitar Kisov <mitko9737@gmail.com>
2025-11-11 15:45:03 +0200

    Added Content.Api with ArticlesController

---
+++ Body4uHUB.Content.Api/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Body4uHUB.Content.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
---
+++ Body4uHUB.Content.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿namespace Body4uHUB.Content.Api.Controllers
+{
+    using Body4uHUB.Content.Application.Commands.Articles.Create;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Threading.Tasks;
+
+    public class ArticlesController : ApiController
+    {
+        /// <summary>
+        /// Create a new article (Trainers and Admins only)
+        /// </summary>
+        [HttpPost]
+        //[Authorize(Policy = "TrainerOrAdmin")]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> CreateArticle(CreateArticleCommand command)
+        {
+            var articleId = await Mediator.Send(command);
+            return Ok(articleId);
+        }
+    }
+}
