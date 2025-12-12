b9de3223db3ed750c34a969e9d4b53cbcf99c2ab
Dimitar Kisov <mitko9737@gmail.com>
2025-12-12 10:18:34 +0200

    Added IServiceRepository and ITrainerProfileRepository

---
+++ Body4uHUB.Services.Domain/Repositories/ITrainerProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using Body4uHUB.Services.Domain.Models;
+using Body4uHUB.Shared;
+
+namespace Body4uHUB.Services.Domain.Repositories
+{
+    public interface ITrainerProfileRepository : IRepository<TrainerProfile>
+    {
+    }
+}
