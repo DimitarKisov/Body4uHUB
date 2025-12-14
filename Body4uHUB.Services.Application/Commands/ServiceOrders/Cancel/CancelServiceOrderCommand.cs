c54db81843bb7ed1b0a5d56616ec7e9c73bd0bf7
Dimitar Kisov <mitko9737@gmail.com>
2025-12-14 17:20:33 +0200

    Added CancelServiceOrderCommand

---
+++ Body4uHUB.Services.Application/Commands/ServiceOrders/Cancel/CancelServiceOrderCommand.cs
@@ -0,0 +1,44 @@
+﻿using Body4uHUB.Services.Domain.Repositories;
+using Body4uHUB.Services.Domain.ValueObjects;
+using Body4uHUB.Shared.Application;
+using Body4uHUB.Shared.Domain;
+using MediatR;
+
+using static Body4uHUB.Services.Domain.Constants.ModelConstants.ServiceOrderConstants;
+
+namespace Body4uHUB.Services.Application.Commands.ServiceOrders.Cancel
+{
+    public class CancelServiceOrderCommand : IRequest<Result>
+    {
+        public int Id { get; set; }
+
+        internal class CancelServiceOrderCommandHandler : IRequestHandler<CancelServiceOrderCommand, Result>
+        {
+            private readonly IServiceOrderRepository _serviceOrderRepository;
+            private readonly IUnitOfWork _unitOfWork;
+
+            public CancelServiceOrderCommandHandler(
+                IServiceOrderRepository serviceOrderRepository,
+                IUnitOfWork unitOfWork)
+            {
+                _serviceOrderRepository = serviceOrderRepository;
+                _unitOfWork = unitOfWork;
+            }
+
+            public async Task<Result> Handle(CancelServiceOrderCommand request, CancellationToken cancellationToken)
+            {
+                var serviceOrder = await _serviceOrderRepository.GetByIdAsync(ServiceOrderId.Create(request.Id), cancellationToken);
+                if (serviceOrder == null)
+                {
+                    return Result.UnprocessableEntity(ServiceOrderNotFound);
+                }
+
+                serviceOrder.Cancel();
+
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                return Result.Success();
+            }
+        }
+    }
+}
---
+++ Body4uHUB.Services.Application/Commands/ServiceOrders/Cancel/CancelServiceOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+using static Body4uHUB.Services.Domain.Constants.ModelConstants.ServiceOrderConstants;
+
+namespace Body4uHUB.Services.Application.Commands.ServiceOrders.Cancel
+{
+    public class CancelServiceOrderCommandValidator : AbstractValidator<CancelServiceOrderCommand>
+    {
+        public CancelServiceOrderCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage(ServiceOrderIdCannotBeZeroOrNegative);
+        }
+    }
+}
