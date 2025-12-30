3f4c97d5f16b4ef1642e32aa3c227332e1531f9a
Dimitar Kisov <mitko9737@gmail.com>
2025-12-30 14:16:45 +0200

    Added Service.Infrastructure layer; Added ServicesDbContext;

---
+++ Body4uHUB.Services.Infrastructure/Persistence/ServicesDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Body4uHUB.Services.Domain.Models;
+using Body4uHUB.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Body4uHUB.Services.Infrastructure.Persistence
+{
+    internal class ServicesDbContext : DbContext
+    {
+        public ServicesDbContext(DbContextOptions<ServicesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrainerProfile> TrainerProfiles { get; set; }
+        public DbSet<ServiceOrder> ServiceOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ServicesDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var modifiedEntries = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified &&
+                            x.Entity is IModifiableEntity);
+
+            foreach (var entry in modifiedEntries)
+            {
+                var entity = (IModifiableEntity)entry.Entity;
+                entity.SetModifiedAt();
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
