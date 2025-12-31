e06a4adbd111624d466ba1f8a49844bcfbe5c4a1
Dimitar Kisov <mitko9737@gmail.com>
2025-12-31 16:05:13 +0200

    Added UnitOfWork and ServiceCollectionExtensions

---
+++ Body4uHUB.Services.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Body4uHUB.Services.Application.Repositories;
+using Body4uHUB.Services.Domain.Repositories;
+using Body4uHUB.Services.Infrastructure.Persistence;
+using Body4uHUB.Services.Infrastructure.Repositories;
+using Body4uHUB.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Body4uHUB.Services.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDbContext<ServicesDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)));
+
+            services
+                .AddScoped<IServiceOrderRepository, ServiceOrderRepository>()
+                .AddScoped<IServiceOrderReadRepository, ServiceOrderReadRepository>()
+                .AddScoped<ITrainerProfileRepository, TrainerProfileRepository>()
+                .AddScoped<ITrainerProfileReadRepository, TrainerProfileReadRepository>()
+                .AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ Body4uHUB.Services.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Body4uHUB.Services.Infrastructure.Persistence;
+using Body4uHUB.Shared.Domain;
+
+namespace Body4uHUB.Services.Infrastructure.Repositories
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly ServicesDbContext _dbContext;
+
+        public UnitOfWork(ServicesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
