febb81410c758fbf5722487305f1740051ef5e8c
Dimitar Kisov <mitko9737@gmail.com>
2026-01-12 15:49:08 +0200

    Added ServiceOrdersController queries endpoints

--- Body4uHUB.Services.Api/Controllers/ServiceOrdersController.cs
+++ Body4uHUB.Services.Api/Controllers/ServiceOrdersController.cs
@@ -2,7 +2,9 @@
 using Body4uHUB.Services.Application.Commands.ServiceOrders.Complete;
 using Body4uHUB.Services.Application.Commands.ServiceOrders.Confirm;
 using Body4uHUB.Services.Application.Commands.ServiceOrders.Create;
+using Body4uHUB.Services.Application.DTOs;
 using Body4uHUB.Services.Application.Queries.ServiceOrders.GetServiceOrderByClients;
+using Body4uHUB.Services.Application.Queries.ServiceOrders.GetServiceOrderById;
 using Body4uHUB.Services.Domain.ValueObjects;
 using Body4uHUB.Shared.Api;
 using Microsoft.AspNetCore.Authorization;
@@ -62,6 +64,7 @@ namespace Body4uHUB.Services.Api.Controllers
         /// Creates a new service order
         /// </summary>
         [HttpPost("create")]
+        [Authorize(Policy = "TrainerOrAdmin")]
         [ProducesResponseType(typeof(ServiceOrderId), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -69,6 +72,45 @@ namespace Body4uHUB.Services.Api.Controllers
         public async Task<IActionResult> CreateServiceOrder(CreateServiceOrderCommand command)
         {
             var result = await Mediator.Send(command);
+            return HandleResult(result);
+        }
+
+        /// <summary>
+        /// Gets service orders by client ID
+        /// </summary>
+        [HttpGet("by-client/{clientId}")]
+        [Authorize]
+        [ProducesResponseType(typeof(IEnumerable<ServiceOrderDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetServiceOrdersByClient(Guid clientId)
+        {
+            var query = new GetOrdersByClientQuery
+            {
+                ClientId = clientId
+            };
+
+            var result = await Mediator.Send(query);
+            return HandleResult(result);
+        }
+
+        /// <summary>
+        /// Gets a service order by its ID
+        /// </summary>
+        [HttpGet("{id}")]
+        [Authorize]
+        [ProducesResponseType(typeof(ServiceOrderDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> GetServiceOrderById(int id)
+        {
+            var query = new GetServiceOrderByIdQuery
+            {
+                Id = id
+            };
+
+            var result = await Mediator.Send(query);
             return HandleResult(result);
         }
     }
