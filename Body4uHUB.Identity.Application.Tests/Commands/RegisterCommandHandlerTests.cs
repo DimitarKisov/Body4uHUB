ac133845a0d6203a8454e5eea5209291ae91465a
Dimitar Kisov <mitko9737@gmail.com>
2026-03-06 15:25:07 +0200

    Added Body4uHUB.Identity.Application.Tests project

---
+++ Body4uHUB.Identity.Application.Tests/Commands/RegisterCommandHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body4uHUB.Identity.Application.Tests.Commands
+{
+    [TestFixture]
+    public class RegisterCommandHandlerTests
+    {
+    }
+}
