05955ff44a23815dbc30d8e257556d11ba6191fe
Dimitar Kisov <mitko9737@gmail.com>
2026-05-20 16:37:02 +0300

    refactor: IForumRepository: change topic id parameters from Guid to int

--- Body4uHUB.Content.Domain/Repositories/IForumRepository.cs
+++ Body4uHUB.Content.Domain/Repositories/IForumRepository.cs
@@ -6,9 +6,9 @@ namespace Body4uHUB.Content.Domain.Repositories
     public interface IForumRepository : IRepository<ForumTopic>
     {
         void Add(ForumTopic forumTopic);
-        Task<ForumTopic> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-        Task<ForumTopic> GetByIdWithPostsAsync(Guid id, CancellationToken cancellationToken = default);
-        Task<bool> ExistsByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<ForumTopic> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<ForumTopic> GetByIdWithPostsAsync(int id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByIdAsync(int id, CancellationToken cancellationToken = default);
         Task<bool> ExistsByTitleAsync(string title, CancellationToken cancellationToken = default);
         void Remove(ForumTopic forumTopic);
     }
