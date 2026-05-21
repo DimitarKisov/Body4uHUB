9db41f2a8ca5438e34d88487b1829f22e5904393
Dimitar Kisov <mitko9737@gmail.com>
2026-05-21 10:49:41 +0300

    chore: tighten BookmarkController authorization and route constraints

--- Body4uHUB.Content.Api/Controllers/BookmarkController.cs
+++ Body4uHUB.Content.Api/Controllers/BookmarkController.cs
@@ -1,26 +1,30 @@
-﻿using Body4uHUB.Content.Api.Extensions;
+using Body4uHUB.Content.Api.Extensions;
 using Body4uHUB.Content.Application.Commands.Bookmarks.AddBookmark;
 using Body4uHUB.Content.Application.Commands.Bookmarks.RemoveBookmark;
 using Body4uHUB.Content.Application.Queries.Bookmarks.GetUserBookmarks;
 using Body4uHUB.Shared.Api;
 using Body4uHUB.Shared.Application;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Body4uHUB.Content.Api.Controllers
 {
+    [Authorize]
     [Route("api/bookmarks")]
     public class BookmarkController : ApiController
     {
         /// <summary>
         /// Add article to bookmarks
         /// </summary>
-        [HttpPost("articles/{articleId}")]
-        [ProducesResponseType(typeof(AddBookmarkResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost("articles/{articleId:int}")]
+        [ProducesResponseType(typeof(AddBookmarkResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
         public async Task<IActionResult> AddBookmark(int articleId)
         {
             var command = new AddBookmarkCommand(User.GetUserId(), articleId);
@@ -46,11 +50,11 @@ namespace Body4uHUB.Content.Api.Controllers
         /// <summary>
         /// Remove article from bookmarks
         /// </summary>
-        [HttpDelete("articles/{articleId}")]
+        [HttpDelete("articles/{articleId:int}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
         public async Task<IActionResult> RemoveBookmark(int articleId)
         {
             var command = new RemoveBookmarkCommand(User.GetUserId(), articleId);
