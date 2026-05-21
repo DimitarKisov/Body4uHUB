7dda7de7e43e83a2149f7c90b3dcd94b1f228c70
Dimitar Kisov <mitko9737@gmail.com>
2026-05-21 11:10:23 +0300

    refactor: Added migration for deleting ArticleNumber from Bookmark entity

---
+++ Body4uHUB.Content.Infrastructure/Persistence/Migrations/20260521080939_RemovedArticleNumber.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Body4uHUB.Content.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedArticleNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bookmarks_ArticleNumber",
+                table: "Bookmarks");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleNumber",
+                table: "Bookmarks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleNumber",
+                table: "Bookmarks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_ArticleNumber",
+                table: "Bookmarks",
+                column: "ArticleNumber");
+        }
+    }
+}
